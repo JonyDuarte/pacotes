222837ffde17731acc0b07581ec3fdf2e0d89f64
JonyDuarte <joaoc84duarte@gmail.com>
2024-10-04 21:26:59 -0300

    v1

---
+++ app.py
@@ -0,0 +1,16 @@
+# bibliotecas
+import platform as p
+import winapps as w
+ 
+ # exibir o sistema operacional do usuário
+print(f"Sistema operacional do usuário: {p.system()} {p.release()}.")
+
+# listas todos os programas instalados no computador
+print("\n Lista de todos os programas instalados: ")
+for programa in w.list_installed():
+    print(f"Programa: {programa.name}.")
+    print(f"Versão: {programa.version}.")
+    print(f"Data da instalação: {programa.install_date}.")
+    print(f"Data de publicação: {programa.publisher}.")
+    print(f"Local de desinstalação: {programa.publisher}.")
+    print(f"{'==-'*30}")
